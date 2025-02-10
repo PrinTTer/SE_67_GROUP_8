bcccbbf96576da48cbc78382c4b6f3b2e8f8ce66
PadThaicreator <namozaa62@gmail.com>
2025-02-10 22:18:03 +0700

    Hello

--- ThaiXplore/front-end-se/src/pages/home_page/home_page.jsx
+++ ThaiXplore/front-end-se/src/pages/home_page/home_page.jsx
@@ -3,6 +3,8 @@ const HomePage = () => {
         <div className="text-9xl text-orange-800">
             Hello World!
             <h1 className="text-amber-600">Hello Im KiT</h1>
+            <h1 className="text-amber-600">Hello Im Namo</h1>
+            
         </div>
     );
 }

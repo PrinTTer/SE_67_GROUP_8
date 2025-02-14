9c274382d82391c7bdd5e8ae208d84803afee661
Suwinai Aiamsumaung <149983646+melisandregod@users.noreply.github.com>
2025-02-15 01:07:38 +0700

    car model

---
+++ ThaiXplore/back-end-se/src/models/car.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const CarSchema = new mongoose.Schema({
+    businessId : {type : String , require : true},
+    carBrand : {type : String , require : true},
+    licensePlate : {type : String , require : true},
+    amountSeat : {type : Number , require : true},
+    price : {type : Number , require : true},
+    totalCars : {type : Number , require : true},
+    bookedDates : [
+        {
+            date : {type : Date ,require : false},
+            booked : {type : Number ,require : false},
+        }
+    ]
+});
+
+export const CarModel = mongoose.model("Car",CarSchema);
+
+export const createCar = (values: Record<string , any>) => new CarModel(values).save().then((car)=>car.toObject());

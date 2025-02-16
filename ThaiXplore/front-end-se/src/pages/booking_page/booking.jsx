d0c1a15b113f2731a2f27d9d1bdf962d93ae8158
KiTSP <sutthipat.p@ku.th>
2025-02-17 00:10:53 +0700

    booking test

--- ThaiXplore/front-end-se/src/pages/booking_page/booking.jsx
+++ ThaiXplore/front-end-se/src/pages/booking_page/booking.jsx
@@ -3,9 +3,11 @@ import { NavBarWithOutText } from "../../component/navbar";
 
 const Booking = () => {
     return(
-        <div className="flex flex-col-reverse lg:flex-row flex-1 items-center justify-center">
+        <div className="flex flex-1">
             <NavBarWithOutText/>
-            <div className="flex flex-5 items-center justify-center h-screen">
+            <div className="flex flex-[1.2] items-center justify-center border-r-1 bg-[#BCD1FB]">
+            </div>
+            <div className="flex flex-[3.8] items-center justify-center border-1">
 
             </div>
         </div>

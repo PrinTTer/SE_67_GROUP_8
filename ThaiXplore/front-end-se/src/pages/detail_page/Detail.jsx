37a652e40f6ddf394cd2cd10794b150185ed6c7e
PadThaicreator <namozaa62@gmail.com>
2025-02-17 03:06:09 +0700

    SideBar_Booking_Food_Hotel

--- ThaiXplore/front-end-se/src/pages/detail_page/Detail.jsx
+++ ThaiXplore/front-end-se/src/pages/detail_page/Detail.jsx
@@ -4,6 +4,7 @@ import {NavBarWithOutText  } from '../../component/navbar' ;
 import {  faPlus,faFileInvoice } from '@fortawesome/free-solid-svg-icons';
 import { getBusiness,getInfo,getBusinessDescription} from '../../data';
 import { faBed,faStar } from '@fortawesome/free-solid-svg-icons';
+import { RightSideBar } from './component/RightBar'
 
 
 
@@ -37,9 +38,9 @@ function Detail() {
                             
                                 
                                   <Link >
-                                      <div className="flex  gap-2 mt-1 shadow-md  rounded-full h-8 w-auto justify-center items-center p-5 mr-2 bg-blue-500">
+                                      <div className="flex  gap-2 mt-1 shadow-md  rounded-full h-8 w-auto justify-center items-center p-5 mr-2 bg-blue-400">
                                         <FontAwesomeIcon icon={faFileInvoice} />
-                                        <p className=" leading-none">Quotation</p>
+                                        <p>Request Quotation</p>
                                       </div>
                                   </Link>
                                 
@@ -47,9 +48,9 @@ function Detail() {
                             
                                 
                                 <Link>
-                                      <div className="flex  gap-2 mt-1 shadow-md  rounded-full h-8 w-auto justify-center items-center p-5 bg-blue-500 mr-1">
+                                      <div className="flex  gap-2 mt-1 shadow-md  rounded-full h-8 w-auto justify-center items-center p-5 bg-blue-400 mr-1">
                                         <FontAwesomeIcon icon={faPlus} />
-                                        <p className=" leading-none">Follow</p>
+                                        <p>Follow</p>
                                       </div>
                                 </Link>
                                 
@@ -73,9 +74,7 @@ function Detail() {
       </div>     
      
       {/* Right Bar */}
-      <div className="flex flex-1 flex-col  items-center border-solid border-l-2 sticky top-0 h-screen">
-        <input type="text" className='bg-amber-50 rounded-4xl border-1 mt-4'/>
-      </div>
+        <RightSideBar type={business.type} />
       
     </div>
   )

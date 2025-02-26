420cbaf1ae876315a1632e17833420d8a9d4867c
Au3 <aum3523@gmail.com>
2025-02-27 01:16:02 +0700

    get user & update users

--- ThaiXplore/back-end-se/src/controllers/user.controller.ts
+++ ThaiXplore/back-end-se/src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
-import { getUsers , deleteUserById } from "../models/users";
+import { get } from "lodash";
+import { getUsers , deleteUserById, getUserById, updateUserById } from "../models/users";
 import express from "express";
 
 export const getAllUsers = async (req:express.Request , res:express.Response):Promise<any> => {
@@ -6,6 +7,43 @@ export const getAllUsers = async (req:express.Request , res:express.Response):Pr
         const users = await getUsers();
         return res.status(200).json(users);
     } catch(err) {
+        console.log(err);
+        return res.sendStatus(400);
+    }
+}
+
+export const getUser = async (req:express.Request , res:express.Response):Promise<any> => {
+    try {
+        const currentUserId:string = get(req , 'identity._id');
+        const user = await getUserById(currentUserId);
+
+        return res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+        return res.sendStatus(400);
+    }
+}
+
+export const updateUser = async (req:express.Request , res:express.Response):Promise<any> => {
+    try {
+        const { email , password , firstName , lastName , address , phoneNumber , role } = req.body;
+        const currentUserId:string = get(req , 'identity._id');
+
+        if(!email || !password || !firstName || !lastName || !phoneNumber || !role ){
+            return res.sendStatus(400);
+        }
+
+         const user = await updateUserById(currentUserId , {
+            email : email,
+            firstName : firstName,
+            lastName : lastName,
+            address : address,
+            phoneNumber : phoneNumber,
+            role : role,
+         })
+
+         return res.status(200).json(user);
+    } catch (err) {
         console.log(err);
         return res.sendStatus(400);
     }
--- ThaiXplore/back-end-se/src/routes/users.routes.ts
+++ ThaiXplore/back-end-se/src/routes/users.routes.ts
@@ -1,9 +1,11 @@
-import { getAllUsers , deleteUser } from "../controllers/user.controller";
+import { getAllUsers , deleteUser, getUser, updateUser } from "../controllers/user.controller";
 import { isAuthenticated } from "../middlewares/isAuthentication.middleware";
 import { isOwner } from "../middlewares/isOwner.middleware";
 import express from "express";
 
 export default (router:express.Router) => {
-    router.get('/users' ,isAuthenticated, getAllUsers);
+    router.get('/all-users' ,isAuthenticated, getAllUsers);
     router.delete('/users/:id' ,isAuthenticated, isOwner , deleteUser);
+    router.get('/users' , isAuthenticated , getUser);
+    router.post('/users' , isAuthenticated , updateUser);
 };

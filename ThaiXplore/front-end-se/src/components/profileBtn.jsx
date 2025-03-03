a97025964bee806b4733ab8cb4e2ce64f2e6431d
Au3 <aum3523@gmail.com>
2025-03-03 21:30:23 +0700

    login

--- ThaiXplore/front-end-se/src/components/profileBtn.jsx
+++ ThaiXplore/front-end-se/src/components/profileBtn.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ProfileIcon } from "./ProfileIcon";
-import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
+import { faEllipsisVertical, faRightLeft, faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import { isAuthenticated } from "../services/authService";
 
 export const ProfileBtn = (prop) => {
@@ -9,16 +9,18 @@ export const ProfileBtn = (prop) => {
     return (
         <div onClick={updateIsOpen} className="flex hover:bg-gray-100 cursor-pointer justify-between gap-10 w-fit h-fit px-5 py-3 rounded-full">
             <div className="flex lg:gap-3">
-                <ProfileIcon user={user} />
                 {
                     !isAuthenticated() ?
-                        (<div className="flex items-center justify-center text-lg font-semibold">
-                            Login / Register
+                        (<div className="flex items-center justify-center">
+                            <FontAwesomeIcon icon={faRightToBracket}/>
                         </div>)
                         :
-                        (<div className="flex items-center justify-center text-lg font-semibold">
-                            {firstName}
-                        </div>)
+                        <>
+                        <ProfileIcon user={user} />
+                        <div className="flex items-center justify-center text-lg font-semibold">
+                            {firstName + " " + user.lastName}
+                        </div>
+                        </>
                 }
             </div>
             <div className="flex items-center">

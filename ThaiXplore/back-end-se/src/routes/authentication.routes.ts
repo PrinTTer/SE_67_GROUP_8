3cec220ab86e28bcc7cf06cbf0e1066ae4cd4ff9
Au3 <aum3523@gmail.com>
2025-03-04 22:29:30 +0700

    Fix

--- ThaiXplore/back-end-se/src/routes/authentication.routes.ts
+++ ThaiXplore/back-end-se/src/routes/authentication.routes.ts
@@ -5,9 +5,5 @@ import express from "express";
 export default (router:express.Router) => {
     router.post('/auth/register' , register);
     router.post('/auth/login' , login);
-<<<<<<< HEAD
     router.post('/auth/logout' , isAuthenticated , logout);
-=======
-    router.post('/auth/logout' , isAuthenticated,logout);
->>>>>>> front-end-namo
 };

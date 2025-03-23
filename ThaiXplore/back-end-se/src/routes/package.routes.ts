33b828a7dcf0ce928e2460ac15875b2df3334ab4
Au3 <aum3523@gmail.com>
2025-03-23 17:21:26 +0700

    get packages

--- ThaiXplore/back-end-se/src/routes/package.routes.ts
+++ ThaiXplore/back-end-se/src/routes/package.routes.ts
@@ -1,11 +1,13 @@
 import express from "express";
-import { deletePackageImage, deletePackages, registerPackage, uploadPackageImages } from "../controllers/package.controller";
+import { deletePackageImage, deletePackages, getAllPackages, getPackage, registerPackage, uploadPackageImages } from "../controllers/package.controller";
 import { isAuthenticated } from "../middlewares/isAuthentication.middleware";
 import { upload } from "../middlewares/uploadFile.middleware";
 
 export default (router: express.Router) => {
+    router.get("/packages" , getAllPackages);
+    router.get("/packages/:packageId" , getPackage);
     router.post("/businesses/:businessId/packages" , isAuthenticated , registerPackage);
-    router.delete("/packages/:packageId" , isAuthenticated , deletePackages);
     router.post("/packages/:packageId/images", isAuthenticated , upload.array("images" , 6) , uploadPackageImages);
+    router.delete("/packages/:packageId" , isAuthenticated , deletePackages);
     router.delete("/packages/:packageId/images/:index", isAuthenticated , deletePackageImage);
 }
